e7091f15e3589017efcbc2151fc4336427d0e7b5
Marcelo Toledo <mtoledo@localhost.localdomain>
2015-10-03 17:12:05 -0300

    Update files

--- rb/spec/pesquisagoogle-versao_exportada_do_selenium_ide_spec.rb
+++ rb/spec/pesquisagoogle-versao_exportada_do_selenium_ide_spec.rb
@@ -17,17 +17,17 @@ describe "PesquisagoogleSpec" do
     @driver.manage.timeouts.implicit_wait = 30
     @verification_errors = []
   end
-  
+
   after(:each) do
     @driver.quit
     @verification_errors.should == []
   end
-  
+
   it "test_pesquisagoogle_spec" do
     @driver.get(@base_url + "/")
-    @driver.find_element(:id, "gbqfq").clear
-    @driver.find_element(:id, "gbqfq").send_keys "Teste de Software"
-    @driver.find_element(:id, "gbqfb").click
+    @driver.find_element(:id, "lst-ib").clear
+    @driver.find_element(:id, "lst-ib").send_keys "Teste de Software"
+    @driver.find_element(:name, "btnG").click
   end
 
   # def element_present?(how, what)
@@ -36,20 +36,20 @@ describe "PesquisagoogleSpec" do
   # rescue Selenium::WebDriver::Error::NoSuchElementError
   #   false
   # end
- 
+
   # def alert_present?()
   #   ${receiver}.switch_to.alert
   #   true
   # rescue Selenium::WebDriver::Error::NoAlertPresentError
   #   false
   # end
-  
+
   # def verify(&blk)
   #   yield
   # rescue ExpectationNotMetError => ex
   #   @verification_errors << ex
   # end
-  
+
   # def close_alert_and_get_its_text(how, what)
   #   alert = ${receiver}.switch_to().alert()
   #   alert_text = alert.text
--- rb/spec/pesquisagoogle_spec.rb
+++ rb/spec/pesquisagoogle_spec.rb
@@ -16,17 +16,17 @@ describe "PesquisagoogleSpec" do
     @base_url = "http://www.google.com.br/"
     @driver.manage.timeouts.implicit_wait = 30
   end
-  
+
   after(:each) do
     @driver.quit
   end
-  
+
   it "test_pesquisagoogle_spec" do
     @driver.get(@base_url + "/")
-    @driver.find_element(:id, "gbqfq").clear
-    @driver.find_element(:id, "gbqfq").send_keys "Teste de Software"
-    @driver.find_element(:id, "gbqfb").click
-    sleep 5
+    @driver.find_element(:id, "lst-ib").clear
+    @driver.find_element(:id, "lst-ib").send_keys "Teste de Software"
+    @driver.find_element(:name, "btnG").click
+    sleep 10
   end
 
 end
